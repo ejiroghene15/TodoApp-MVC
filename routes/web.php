b719ad29c6452be939c80a49355220f3ffd65157
Ejiroghene Obiuwevbi <ejiroghene15@gmail.com>
2022-12-16 12:23:35 +0100

    redirect to login automatically

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', redirect('/login'));
+
+Route::redirect('/', '/login');
 
 Route::get('login', [AuthController::class, 'login'])->name("login");
 
