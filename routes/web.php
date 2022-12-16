8a9da9368c4fcadb3ccf4c6c3b7c977bd4d277d4
Ejiroghene Obiuwevbi <ejiroghene15@gmail.com>
2022-12-16 12:20:24 +0100

    redirect the user to login page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', redirect()->route('login'));
+
 Route::get('login', [AuthController::class, 'login'])->name("login");
 
 Route::post('login', [AuthController::class, 'validateLogin'])->name("sign_in");
