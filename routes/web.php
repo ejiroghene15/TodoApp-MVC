33ee41d7a2830e3f797833a2f91c16f1a28880fe
Ejiroghene Obiuwevbi <ejiroghene15@gmail.com>
2022-12-16 12:21:55 +0100

    redirect index to login page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', redirect()->route('login'));
+Route::get('/', redirect('/login'));
 
 Route::get('login', [AuthController::class, 'login'])->name("login");
 
